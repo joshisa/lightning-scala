64f0638617c636123e03c5cbbd000dacf3d956dd
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-18 21:35:42 -0400

    Update Plots.scala

--- src/main/scala/org/viz/lightning/types/Plots.scala
+++ src/main/scala/org/viz/lightning/types/Plots.scala
@@ -56,7 +56,7 @@ trait Plots extends Base {
   /**
    * Histogram support.
    */
-  def histogram(values: Array[Array[Double]],
+  def histogram(values: Array[Double],
                 label: Array[Int] = Array[Int](),
                 xaxis: String = ""): Visualization = {
 
