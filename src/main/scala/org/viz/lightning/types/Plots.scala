b677b473a5e81e36d7589ba028c5890b2be82972
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-20 17:59:13 -0400

    Update Plots.scala

--- src/main/scala/org/viz/lightning/types/Plots.scala
+++ src/main/scala/org/viz/lightning/types/Plots.scala
@@ -95,7 +95,7 @@ trait Plots extends Base {
    */
   def scatter(x: Array[Double],
               y: Array[Double],
-              label: Array[Int] = Array[Int](),
+              group: Array[Int] = Array[Int](),
               value: Array[Double] = Array[Double](),
               colormap: String = "",
               size: Array[Double] = Array[Double](),
@@ -104,7 +104,7 @@ trait Plots extends Base {
               yaxis: String = ""): Visualization = {
 
     val points = Utils.getPoints(x, y)
-    val data = Map("points" -> points.toList, "group" -> label.toList)
+    val data = Map("points" -> points.toList, "group" -> group.toList)
 
     val settings = new Settings()
       .append(List(Label(label), Value(value), Colormap(colormap), Size(size), Alpha(alpha)))
